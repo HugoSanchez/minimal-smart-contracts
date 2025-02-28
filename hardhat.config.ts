6b64b0ab1e58f1d0e9bab5a6283bf0a934e294ed
HugoSanchez <hugosanchez251@gmail.com>
2025-02-28 11:03:54 +0100

    contracts depoloyed to Optimism Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 require("@nomiclabs/hardhat-ethers");
 require('@openzeppelin/hardhat-upgrades');
 
+dotenv.config();
 
 const config: HardhatUserConfig = {
 	solidity: {
@@ -13,7 +15,18 @@ const config: HardhatUserConfig = {
 			runs: 200
 		  }
 		}
-	  }
+	},
+	networks: {
+		optimismSepolia: {
+			url: `https://opt-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
+	etherscan: {
+		apiKey: {
+			"optimism-sepolia": process.env.OPTIMISM_ETHERSCAN_API_KEY || "",
+		},
+	},
 }
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,42 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+	const [deployer] = await ethers.getSigners();
+	console.log("Deploying contracts with the account:", deployer.address);
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+	// Deploy URIEncoding library
+	const URIEncoding = await ethers.getContractFactory("URIEncoding");
+	const uriEncoding = await URIEncoding.deploy();
+	await uriEncoding.deployed();
+	console.log("URIEncoding deployed to:", uriEncoding.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	// Deploy MinimalCollection with URIEncoding library
+	const MinimalCollection = await ethers.getContractFactory("MinimalCollection", {
+		libraries: {
+		URIEncoding: uriEncoding.address,
+		},
+	});
+	const minimalCollection = await MinimalCollection.deploy();
+	await minimalCollection.deployed();
+	console.log("MinimalCollection deployed to:", minimalCollection.address);
 
-  await lock.deployed();
+	// Deploy MinimalMarket
+	const MinimalMarket = await ethers.getContractFactory("MinimalMarket");
+	const market = await MinimalMarket.deploy();
+	await market.deployed();
+	await market.initialize(deployer.address, deployer.address); // Replace second parameter with your fee destination address
+	console.log("MinimalMarket deployed to:", market.address);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	// Deploy CollectionFactory
+	const CollectionFactory = await ethers.getContractFactory("CollectionFactory");
+	const factory = await CollectionFactory.deploy(minimalCollection.address);
+	await factory.deployed();
+	console.log("CollectionFactory deployed to:", factory.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
 });
